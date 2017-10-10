36ba09a6722567877822cac87633d621161f3eda
Jesus Cardona <jayrage@Jesuss-MacBook-Pro.local>
2017-10-10 10:43:04 -0500

    added events ;er the commit message for your changes. Lines starting

--- ngApp/app.js
+++ ngApp/app.js
@@ -1,1 +1,28 @@
-let app = angular.module("eventsApp",["ngResource"])
+let app = angular.module("eventsApp",['ngResource','ui.router']);
+
+app.config(($stateProvider) => {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: '/ngApp/views/home.html'
+    })
+    .state('contact', {
+      url: '/contact',
+      templateUrl: '/ngApp/views/contact.html'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: '/ngApp/views/about.html'
+    })
+    .state('addEvent', {
+      url: '/addEvent',
+      templateUrl: '/ngApp/views/addevent.html'
+    })
+    .state('maps', {
+      url: '/maps',
+      templateUrl: '/ngApp/views/maps.html'
+    })
+
+  // $routerProvider.otherwise('/');
+  // $locationProvider.html5Mode(true);
+});
